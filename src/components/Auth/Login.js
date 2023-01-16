5af932807726c314eeabd130fad07e517c26bdd4
nataliekuechler <nataliekuechler@outlook.com>
2023-01-15 23:00:39 -0600

    updated styling

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,12 +1,11 @@
 import React from 'react'
+import Countdown from 'react-countdown'
+
 import '../Auth/Login.css'
-//Below we import useAuth from our AuthContext in order to access the login() function
-//useAuth() is the function we want to import any time we need currentUser, login(), or logout()
+
 import { useAuth } from '../../contexts/AuthContext'
 import { useNavigate } from 'react-router-dom'
 import { Carousel, Container, Card} from 'react-bootstrap'
-
-
 
 import image from '../../images/carousel1.JPG'
 import image2 from '../../images/carousel2.JPG'
@@ -15,10 +14,18 @@ import image4 from '../../images/carousel4.JPG'
 
 
 
+
 export default function Login() {
 
     const { login } = useAuth()
     const navigate = useNavigate()
+    
+    const CoundownRenderer = ({ days, hours, minutes, seconds }) => (
+        <>
+          <h4>{days} days {hours} hours, {minutes} minutes, {seconds} seconds<br />
+          Until we say "I do" &hearts;</h4>
+        </>
+      )
 
     async function handleAuth() {
         //Await keyword will pause any more code from executing until we get a response back from Firebase
@@ -26,12 +33,43 @@ export default function Login() {
 
         //return the user to a specific location using useNavigate from react-router-dom
         return navigate('/')
-
+    
     }
 
+    
+
   return (
-    <div className='login row'>
-        <div className='col m-3'>
+
+
+    <div className='login row mx-5 p-1'>
+        <div className='col-sm-5 my-3 mx-4'>
+                <Container> 
+                    <Card className='counter mx-auto mt-2 border-dark text-center'>
+                        <Card.Header className='bg-dark text-white'>
+                            <h1>Eddie & Natalie</h1>
+                        </Card.Header>
+                        <Card.Body>
+                        <div>
+                            <h3>May 30, 2023 | Blue Springs, MO</h3>
+                            <Countdown date={Date.now() + 11577600000} renderer={CoundownRenderer} />
+                        </div>
+                        </Card.Body>
+                    </Card>
+                    <Card className='loginCard mx-auto border-dark text-center'>
+                <Card.Header className='bg-dark text-white'>
+                    <h1>Login</h1>
+                </Card.Header>
+                <Card.Body>
+                    <p>To gain access to our wedding planner app, please login below.</p>
+                    <button className="btn m-1" onClick={() => handleAuth() }>
+                        Login with Github
+                    </button>
+                    <br />
+                </Card.Body>
+            </Card>
+                </Container>
+            </div>
+        <div className='col-sm-6 my-3'>
         <Carousel controls={true} fade>
                 <Carousel.Item>
                     {/* image and .Caption are placed inside the .Item */}
@@ -70,44 +108,7 @@ export default function Login() {
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
-            </div>
-        <div className='col'>
-        <Container>
-            <Card className='loginCard mx-auto mt-2 border-dark text-center'>
-                <Card.Header className='bg-dark text-white'>
-                    <h3>Login</h3>
-                </Card.Header>
-                <Card.Body>
-                    <p>To gain access to our wedding planner app, please login below.</p>
-                    <button className="btn btn-success m-1" onClick={() => handleAuth() }>
-                        Login with Github
-                    </button>
-                    <br />
-                    <button className="btn btn-success m-1" onClick={() => handleAuth() }>
-                        Login with Email
-                    </button>
-                </Card.Body>
-            </Card>
-            <Card className='counter mx-auto mt-2 border-dark text-center'>
-                <Card.Header className='bg-dark text-white'>
-                    <h3>Counter</h3>
-                </Card.Header>
-                <Card.Body>
-                <div>
-                    Insert Countdown to Wedding HERE!!
-                    Here's some text to fill the space for now
-                    Here's some text to fill the space for now
-                    Here's some text to fill the space for now
-                    Here's some text to fill the space for now
-                    Here's some text to fill the space for now
-                    Here's some text to fill the space for now
-                    Here's some text to fill the space for now
-                </div>
-
-                </Card.Body>
-            </Card>
-        </Container>
-        </div>
+            </div>     
     </div>
   )
 }
